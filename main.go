cd1a2f12156a5bbf3f9a8485788e64d5432f1647
Emre Kılıç <emre1kilic@outlook.com>
2022-12-04 21:13:05 +0300

    solve deadlock

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -19,21 +20,25 @@ func main() {
 	keyword := "love"
 
 	lines := readFileByLines(fileName)
+	// CONCURRENT OPERATION
+	concurrentOp(lines, keyword)
 	// Calculate elapsed time
 	startTime := time.Now()
-	concurrentOperation(lines, keyword)
+
 	elapsedTime := time.Since(startTime)
 
 	// print results
 	fmt.Printf("Elapsed Time:%d ns\n ", elapsedTime.Nanoseconds())
-	
+	fmt.Println(matchedIndices)
 }
 
-func concurrentOperation(lines []string, keyword string) {
+func concurrentOp(lines []string, keyword string) {
+	var wg sync.WaitGroup /* SYNCHRONIZE goroutines */
+
 	linesLen := len(lines)
-	
+
 	// Make the line length even
-	if linesLen%2 != 0 { 
+	if linesLen%2 != 0 {
 		lines = append(lines, complementWord)
 		linesLen += 1
 	}
@@ -43,15 +48,21 @@ func concurrentOperation(lines []string, keyword string) {
 	endIndx := offset
 
 	for i := 0; i < threadCount; i++ {
-		go getMatchedLinesIndices(keyword, lines, startIdx, endIndx)
+		fmt.Printf("StartIdx: %d - EndIdx: %d\n", startIdx, endIndx)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			getMatchedLinesIndices(keyword, lines, startIdx, endIndx)
+
+		}()
 		startIdx += offset
 		endIndx += offset
 	}
+	wg.Wait()
 }
 
 // Read file content line by line
 func readFileByLines(fname string) []string {
-
 	file, err := os.Open(fname)
 	if err != nil {
 		log.Fatal("Error opening file", err)
@@ -75,6 +86,6 @@ func getMatchedLinesIndices(keyword string, lines []string, from int, to int) []
 		}
 	}
 	fmt.Println(lineIndices)
-	//matchedIndices = append(matchedIndices, lineIndices...)
+	matchedIndices = append(matchedIndices, lineIndices...)
 	return lineIndices
 }
